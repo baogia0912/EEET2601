50bf26717c0957f6ec2dc516a245522e0cf4bb41
baogia0912 <51979397+baogia0912@users.noreply.github.com>
2021-11-03 15:31:30 +0700

    excercise 5

--- week3 lab/ex5.c
+++ week3 lab/ex5.c
@@ -7,6 +7,8 @@ int main() {
     scanf("%d", &input);
 
     for (int i = input; i > 0; i--) {
-        
+        for (int j = i; j > 0; j--) printf("%d ", j);
+        for (int j = 2; j <= i; j++) printf("%d ", j);
+        printf("\n");
     }
 }
